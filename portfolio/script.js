751f20aef1a5c3f7d30bf6801c97b95d42ddc6d9
alamis <sophian.alami-chentoufi@etu.umontpellier.fr>
2026-03-06 22:06:29 +0100

    fix de bug

--- portfolio/script.js
+++ portfolio/script.js
@@ -50,3 +50,14 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+
+document.querySelectorAll(".accordion-item").forEach(item => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
+
+
